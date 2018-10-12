1331637a6a8c4ef66ea5f82e2ed212db412b8be3
Oliver Caldwell <olliec87@gmail.com>
2018-10-12 16:22:47 +0100

    Should be a working version, not quite

--- src/clojure/wakeup/main.clj
+++ src/clojure/wakeup/main.clj
@@ -1,5 +1,44 @@
 (ns wakeup.main
-  (:import [wakeup.porcupine Porcupine]))
+  (:import [wakeup.porcupine Porcupine]
+
+           ;; These are required for the microphone input.
+           [javax.sound.sampled AudioFormat DataLine TargetDataLine AudioSystem]))
+
+;; Notes on audio formats:
+;; Discord provides audio as `48KHz 16bit stereo signed BigEndian PCM`.
+;; Porcupine requires `16KHz 16bit mono signed LittleEndian PCM` but in 512 length short-array frames (a short is two bytes).
+;; GCP speech recognition requires the same as Porcupine but as byte pairs and without the 512 frames.
+
+(defn init-porcupine []
+  (Porcupine. "wake-word-engine/Porcupine/lib/common/porcupine_params.pv"
+              "wake-word-engine/wake_phrase.ppn"
+              0.5))
+
+;; Adapted from: https://gist.github.com/BurkeB/ebf5f01c0d20ff6b9dc111ac427ddea8
+(defn with-microphone [f]
+  (let [audio-format (new AudioFormat 16000 16 1 true true)
+        info (new javax.sound.sampled.DataLine$Info TargetDataLine audio-format)]
+
+    (when-not (AudioSystem/isLineSupported info)
+      (throw (Error. "AudioSystem/isLineSupported returned false")))
+
+    (with-open [line (AudioSystem/getTargetDataLine audio-format)]
+      (doto line
+        (.open audio-format)
+        (.start))
+
+      (f line))))
 
 (defn -main []
-  (println "Hello, World!"))
+  (println "Starting up wake word detector...")
+  (let [porcupine (init-porcupine)]
+    (with-microphone
+      (fn [line]
+        (let [size 512
+              buf (byte-array size)]
+          (println "Listening!")
+          (loop []
+            (.read line buf 0 size)
+            (when (.processFrame porcupine (short-array buf))
+              (println "Wake word detected."))
+            (recur)))))))
